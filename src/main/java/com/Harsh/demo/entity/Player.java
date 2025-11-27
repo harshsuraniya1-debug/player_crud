ef865f1104f2896e9627e3a1851433ef6580e77d
harshsuraniya1-debug <harshsuraniya1@gmail.com>
2025-11-27 13:14:51 +0530

    Entity file

--- src/main/java/com/Harsh/demo/entity/Player.java
+++ src/main/java/com/Harsh/demo/entity/Player.java
@@ -2,10 +2,10 @@ package com.Harsh.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 @Entity
 @Getter
